336c4067f4a6583c26d5e87f7b85b8fb94cf75b0
Alejandro Riveros <lar4892@gmail.com>
2018-12-03 07:55:25 -0400

    master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sorteo-angular-simple';
+  valor1: number;
+  valor2: number;
+  valor3: number;
+  resultado: string;
+  constructor(){
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+  }
+  retornarAleatorio(){
+    return Math.trunc(Math.random() * 6) + 1;
+  }
+  jugar(){
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+    if (this.valor1==this.valor2 && this.valor1==this.valor3)
+      this.resultado='Ganó';
+    else
+    this.resultado='Perdió';
+  }
 }
